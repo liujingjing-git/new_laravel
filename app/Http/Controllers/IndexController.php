65ceaea2f963711f42217a6efd40ba560855624c
kwl <kwl@localhost.localdomain>
2020-07-21 06:31:45 +0000

    创建model

---
+++ app/Http/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Model\GoodsModel;
+
+
+class IndexController extends Controller
+{
+    public function index(){
+        $list=GoodsModel::get();
+        dd($list);
+        return view('index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
 
-//登录注册 
+
+//登录注册
 Route::get('/user/reg','UserController@register'); //注册
 Route::post('/user/regdo','UserController@register_do'); //执行注册
 Route::get('/user/login','UserController@login'); //登录
 Route::post('/user/logindo','UserController@login_do'); //执行登录
+
+//首页
+Route::get('/','IndexController@index');
+
 
 //购物车
 Route::get('/cart','CartController@cart'); //购物车
@@ -30,3 +32,5 @@ Route::get('/product','CartController@product'); //商品列表
 //我的收藏
 Route::get('/collection','ListController@collection'); //收藏页
 Route::get('blog','ListController@blog');//;历史记录
+
+
