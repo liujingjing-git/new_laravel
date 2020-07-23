cdf0286c2dc679753607f7c94f56f02bc2482eeb
smile <smile@localhost.localdomain>
2020-07-23 11:52:12 +0000

    搜索

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -28,9 +28,16 @@ class CartController extends Controller
     /**
      * 商品列表
      */
-    public function product()
+    public function product(Request $request)
     {
-       $prolist=GoodsModel::orderby('goods_id','DESC')->limit(6)->get();
-        return view('cart/product',['prolist'=>$prolist]);
+        $goods_name = $request->input('goods_name');
+
+        $where = [];
+        if(!empty($goods_name)){
+            $where[]=['goods_name','like',"%$goods_name%"];
+        }
+
+        $prolist=GoodsModel::where($where)->orderby('goods_id','DESC')->limit(6)->get();
+        return view('cart/product',['prolist'=>$prolist,'goods_name'=>$goods_name]);
     }
 }
